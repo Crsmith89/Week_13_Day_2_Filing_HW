5a5d0f06ebc135109e091612a965a6431588dc0d
Craig Smith <craig_ag_smith@outlook.com>
2021-09-07 20:17:55 +0100

    complete

--- src/main/java/com/codeclan/example/folderservice/components/DataLoader.java
+++ src/main/java/com/codeclan/example/folderservice/components/DataLoader.java
@@ -1,7 +1,11 @@
 package com.codeclan.example.folderservice.components;
 
 
+import com.codeclan.example.folderservice.models.File;
+import com.codeclan.example.folderservice.models.Folder;
 import com.codeclan.example.folderservice.models.User;
+import com.codeclan.example.folderservice.repository.FileRepository;
+import com.codeclan.example.folderservice.repository.FolderRepository;
 import com.codeclan.example.folderservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -14,6 +18,12 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    FileRepository fileRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
 
 
     public DataLoader(){
@@ -22,12 +32,42 @@ public class DataLoader implements ApplicationRunner {
 
     public void run(ApplicationArguments args){
         User user1 = new User("Jack");
-        User user2 = new User("Victor");
-        User user3 = new User("Winston");
-
         userRepository.save(user1);
+
+        User user2 = new User("Victor");
         userRepository.save(user2);
+
+        User user3 = new User("Winston");
         userRepository.save(user3);
 
+        Folder folder1 = new Folder("Holiday pictures", user1);
+        folderRepository.save(folder1);
+
+        Folder folder2 = new Folder("Music", user1);
+        folderRepository.save(folder2);
+
+        Folder folder3 = new Folder("CV", user2);
+        folderRepository.save(folder3);
+
+        Folder folder4 = new Folder("Dog Pictures", user3);
+        folderRepository.save(folder4);
+
+
+        File file1 = new File("New York", ".jpeg", 156, folder1);
+        fileRepository.save(file1);
+
+        File file2 = new File("90's Dance", "mp3", 256, folder2);
+        fileRepository.save(file2);
+
+        File file3 = new File("Tam CV", ".txt", 56, folder3);
+        fileRepository.save(file3);
+
+        File file4 = new File("Buddy", ".png", 516, folder4);
+        fileRepository.save(file4);
+
+
+
     }
+
+
 }
